999b090ae097f878b80c8e2cdffc4a08fc1ad9fe
Jason <cameljeet@gmail.com>
2015-01-20 20:39:49 -0800

    Completed Create - defined the new action in the projects controller - added form to new.html.erb - set a default due date in the controller - defined the create action   - added if statement to be used for error checking. to be implemented...

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,30 @@
 class ProjectsController < ApplicationController
+  def index
+  end
+
+  def create
+    @project = Project.new params.require(:project).permit(:title, :description, :due_date)
+    if @project.save
+      redirect_to
+    else
+      render :new
+    end
+    # render text: params
+  end
+
+  def new
+    @project = Project.new due_date: (Date.today + 7.day)
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
 
+  # get 'projects/index'
+  # get 'projects/create'
+  # get 'projects/new'
+  # get 'projects/edit'
+  # get 'projects/show'
+  # get 'projects/update'
+  # get 'projects/destroy'
+
+  
   get "home/about" => "home#about"
+  # get "/about" => "home#about" # Is this right?
   root "home#index"
-  resources :home
+  # resources :home # is this necessary?
+  resources :projects
 
 end
