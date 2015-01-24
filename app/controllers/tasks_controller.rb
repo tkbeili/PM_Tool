c2dbd233ecc3f96c094933fec700efac61aa85be
Jason <cameljeet@gmail.com>
2015-01-23 19:21:21 -0800

    CRUD on Tasks completed - title of the task links to the edit page - edit page contains the link to delete - the create task for is on a partial page - delete only appears for the edit, doesnt show for new. Thanks to Brent.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,19 +12,22 @@ class TasksController < ApplicationController
     if @task.save
       redirect_to tasks_path
     else
-      render text: params
+      render :new
     end
-  end
-
-  def show
+    # render text: params
   end
 
   def edit
+    @task = Task.find params[:id]
   end
 
   def update
+    @task = Task.find params[:id]
   end
 
   def destroy
+    @task = Task.find params[:id]
+    @task.destroy
+    redirect_to tasks_path
   end
 end
