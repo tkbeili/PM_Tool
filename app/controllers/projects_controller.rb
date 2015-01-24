7abc2e428fb9dd9a28ad16fdea28df74c0ced5f7
Jason <cameljeet@gmail.com>
2015-01-23 22:13:07 -0800

    PM Tool: Pagination - Pagination added with will_paginate gem - Will work on pagination with out the gem.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,9 +4,9 @@ class ProjectsController < ApplicationController
     # @projects = Project.all
 
     if params[:search]
-      @projects = Project.search(params[:search]).order("created_at DESC")
+      @projects = Project.search(params[:search]).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
     else
-      @projects = Project.all.order('created_at DESC')
+      @projects = Project.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 10)
     end
   end
 
