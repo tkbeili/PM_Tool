aca8e945b2385ceaa59c9195a3716201fbea9549
Jason <cameljeet@gmail.com>
2015-01-24 23:31:36 -0800

    Complete Create and Read for Discussion - validations for discussion improved - change project title to PM Tool - added error checking to discussion submission - all discussions showing on project/show page

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,10 +1,15 @@
 class DiscussionsController < ApplicationController
 
   def create
+    @project = Project.find params[:project_id]
     @discussion = Discussion.new params.require(:discussion).permit(:title, :description)
     @discussion.project_id = params[:project_id]
-    @discussion.save
-    render text: params
+    if @discussion.save
+      redirect_to project_path(@project)
+    else
+      render "projects/show"
+      # render text: params
+    end
   end
 
 end
--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,6 +1,7 @@
 class Discussion < ActiveRecord::Base
   belongs_to :project
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: { case_sensitive: false }
+  validates :description, presence: true
 end
 
 
